e08999297d7341c737ccb21b07edaedf0abbf430
mormoli <mormoli@yahoo.gr>
2018-01-18 23:56:35 +0200

    initial commit

--- Lesson07-Waitlist/T07.06-Exercise-RemoveGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
+++ Lesson07-Waitlist/T07.06-Exercise-RemoveGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
@@ -22,7 +22,7 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
      * @param context the calling context/activity
      * @param cursor the db cursor with waitlist data to display
      */
-    public GuestListAdapter(Context context, Cursor cursor) {
+    GuestListAdapter(Context context, Cursor cursor) {
         this.mContext = context;
         this.mCursor = cursor;
     }
@@ -45,12 +45,13 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
         String name = mCursor.getString(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME));
         int partySize = mCursor.getInt(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE));
         // TODO (6) Retrieve the id from the cursor and
-
+        long id = mCursor.getLong(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry._ID));
         // Display the guest name
         holder.nameTextView.setText(name);
         // Display the party count
         holder.partySizeTextView.setText(String.valueOf(partySize));
         // TODO (7) Set the tag of the itemview in the holder to the id
+        holder.itemView.setTag(id);
     }
 
 
@@ -65,7 +66,7 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
      *
      * @param newCursor the new cursor that will replace the existing one
      */
-    public void swapCursor(Cursor newCursor) {
+    void swapCursor(Cursor newCursor) {
         // Always close the previous mCursor first
         if (mCursor != null) mCursor.close();
         mCursor = newCursor;
@@ -92,10 +93,10 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
          * @param itemView The View that you inflated in
          *                 {@link GuestListAdapter#onCreateViewHolder(ViewGroup, int)}
          */
-        public GuestViewHolder(View itemView) {
+        GuestViewHolder(View itemView) {
             super(itemView);
-            nameTextView = (TextView) itemView.findViewById(R.id.name_text_view);
-            partySizeTextView = (TextView) itemView.findViewById(R.id.party_size_text_view);
+            nameTextView = itemView.findViewById(R.id.name_text_view);
+            partySizeTextView = itemView.findViewById(R.id.party_size_text_view);
         }
 
     }
